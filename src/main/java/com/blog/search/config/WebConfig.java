5a4ce53aa24bf5110136ea96e5c5cfa53dfffcd7
K-MHOON <azmria117@gmail.com>
2023-03-19 17:56:12 +0900

    feat : RestTemplate, ServiceLocator Bean 등록

--- src/main/java/com/blog/search/config/WebConfig.java
+++ src/main/java/com/blog/search/config/WebConfig.java
@@ -1,11 +1,18 @@
 package com.blog.search.config;
 
 import com.blog.search.converter.StringToSearchSortConverter;
+import org.springframework.beans.factory.config.ServiceLocatorFactoryBean;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -18,5 +25,21 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .requestFactory(() -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
+                .setConnectTimeout(Duration.ofMillis(5000))
+                .setReadTimeout(Duration.ofMillis(5000))
+                .build();
+    }
+
+    @Bean
+    public ServiceLocatorFactoryBean serviceLocatorForPaymentHandlerLocator() {
+        ServiceLocatorFactoryBean factoryBean = new ServiceLocatorFactoryBean();
+        factoryBean.setServiceLocatorInterface(OpenApiInfoLocator.class);
+        return factoryBean;
     }
 }
