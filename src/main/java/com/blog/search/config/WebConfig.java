207d646eed69ede428bdb9cf0c0fe76a70c49bab
K-MHOON <azmria117@gmail.com>
2023-03-20 20:22:11 +0900

    fix : restTemplate 설정 rollback

--- src/main/java/com/blog/search/config/WebConfig.java
+++ src/main/java/com/blog/search/config/WebConfig.java
@@ -1,16 +1,13 @@
 package com.blog.search.config;
 
 import com.blog.search.converter.StringToSearchSortConverter;
-import org.apache.hc.client5.http.classic.HttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.springframework.beans.factory.config.ServiceLocatorFactoryBean;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -32,16 +29,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-        PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager();
-        poolingHttpClientConnectionManager.setMaxTotal(100);
-        poolingHttpClientConnectionManager.setDefaultMaxPerRoute(20);
-
-        HttpClient httpClient = HttpClientBuilder.create()
-                .setConnectionManager(poolingHttpClientConnectionManager)
-                .build();
-
         return restTemplateBuilder
-                .requestFactory(() -> new HttpComponentsClientHttpRequestFactory(httpClient))
+                .requestFactory(() -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
                 .setConnectTimeout(Duration.ofMillis(5000))
                 .setReadTimeout(Duration.ofMillis(5000))
                 .build();
