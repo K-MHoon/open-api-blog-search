d957c8a6e6043e0411927fea91c9cdd5eb68734f
K-MHOON <azmria117@gmail.com>
2023-03-21 23:08:23 +0900

    feat : 네이버 블로그 검색 파라미터 추가 및 검증 추가

--- src/main/java/com/blog/search/dto/request/openapi/kakao/KakaoBlogSearchParameter.java
+++ src/main/java/com/blog/search/dto/request/openapi/kakao/KakaoBlogSearchParameter.java
@@ -1,47 +1,51 @@
 package com.blog.search.dto.request.openapi.kakao;
 
 import com.blog.search.dto.request.openapi.OpenApiRequestParameter;
-import com.blog.search.enums.sort.SearchSort;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
+import com.blog.search.enums.sort.KakaoBlogSearchSort;
+import com.blog.search.enums.sort.NaverBlogSearchSort;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Objects;
 
-@Getter
-@ToString
-public class OpenApiRequestParameterKakaoBlogSearch implements OpenApiRequestParameter {
+@Data
+public class KakaoBlogSearchParameter implements OpenApiRequestParameter {
 
+    @NotNull
     private String query;
-    private SearchSort sort;
+
+    private KakaoBlogSearchSort sort;
+
+    @Min(1)
+    @Max(50)
     private Integer page;
+
+    @Min(1)
+    @Max(50)
     private Integer size;
 
     @Builder
-    public OpenApiRequestParameterKakaoBlogSearch(String query, SearchSort sort, Integer page, Integer size) {
-        this.query = Objects.requireNonNull(query, "검색어는 필수 값입니다.");
+    public KakaoBlogSearchParameter(String query, KakaoBlogSearchSort sort, Integer page, Integer size) {
+        this.query = query;
         this.sort = sort;
         this.page = page;
         this.size = size;
     }
 
     @Override
-    public String getUrlWithParams(String host, String url) {
+    public UriComponentsBuilder getParameters() {
         return UriComponentsBuilder
                 .newInstance()
-                .scheme("https")
-                .host(host)
-                .path(url)
                 .queryParam("query", this.getQuery())
                 .queryParam("sort", getSortValue())
                 .queryParam("page", this.getPage())
-                .queryParam("size", this.getSize())
-                .build()
-                .toUriString();
+                .queryParam("size", this.getSize());
     }
 
-    private Object getSortValue() {
+    private String getSortValue() {
         return Objects.isNull(this.getSort())
                 ? null
                 : this.getSort().getValue();
